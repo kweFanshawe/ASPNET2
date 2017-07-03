33b2ab43d061780664c0764b31254d52b62d003d
kweFanshawe <k_we@fanshaweonline.ca>
2017-07-03 16:06:20 -0400

    Update and rename .js to casestudykidsShoes.js

---
+++ casestudykidsShoes.js
@@ -0,0 +1,11 @@
+[
+   {
+      "COLORS":"RED",
+      "PRICE":"30",
+      "ORIGINALPRICE":"45",
+      "AVAILABLE":"true",
+      "SIZE":"3.5M, 4.5M, 5.5M",
+	  "BRAND" : "NIKE",
+      "ITEM":"Free Run 2017 Sneaker",
+   }
+]
